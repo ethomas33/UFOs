6f9aac9f449d26604344d082cc5aafd094ec7215
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-04-04 14:27:52 -0400

    11.3.2

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3 -- declare the variable tbody and then use d3 select to tell javascript to look for the tbody tag in the html
+var tbody = d3.select("tbody");
+
