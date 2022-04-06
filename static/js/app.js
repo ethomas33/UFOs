8f34866f7bbffd16f997e540c8b9b04dd00a692a
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-04-06 11:30:31 -0400

    11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,29 @@ data.forEach((dataRow)=>{
         }
     );
 });
+
+function handleClick() {
+    //grab the date time value from the filter
+    //.select selects the first item in selector string - #datetime 
+    // .property("value") tells it to actually grabe it and hold it in the date variable
+    let date = d3.select("#datetime").property("value");
+    // tableData is the original raw data
+    let filterData = tableData
+
+    //check to see if ta date was entered and filter the data using the date
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);     
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
