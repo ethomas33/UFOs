3aafbe9e7650c4b02fb2fb105719df677a3ae4b1
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-04-09 06:52:37 -0400

    notes added

--- static/js/app.js
+++ static/js/app.js
@@ -26,23 +26,26 @@ function buildTable(data) {
 // 1. Create a variable to keep track of all the filters as an object.
 var filters = {};
 
-// 3. Use this function to update the filters. 
+// 3. Use this function to update the filters (triggered by line 70 below). 
 function updateFilters() {
 
-    // 4a. Save the element that was changed as a variable.
+  // 4a. Save the element that was changed as a variable.
   let changedElement = d3.select(this);
-    // 4b. Save the value that was changed as a variable.
-  let elementValue = changedElement.property("value");
-  console.log(elementValue)
-    // 4c. Save the id of the filter that was changed as a variable.
+  
+  // 4c. Save the id (ID coming from html) of the filter that was changed as a variable.
   let filterID = changedElement.attr("id");
-  console.log(elementValue)
+  
+  // 4b. Save the value (coming from user input) that was changed as a variable.
+  let elementValue = changedElement.property("value");
+  //console.log(elementValue)
+
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
   if (elementValue){
     filters[filterID] = elementValue;
   }
+  //removes key and value 
   else{
     delete filters[filterID];
   }
@@ -57,11 +60,13 @@ function updateFilters() {
   
     // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
-  
+    
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.entries(filters).forEach(([key,value]) => {
-      filteredData = filteredData.filter(row => row[key] === value);
+    Object.entries(filters).forEach(([id,value]) => {
+      //console.log(id)
+      //console.log(value)
+      filteredData = filteredData.filter(row => row[id] === value);
     });  
   
     // 10. Finally, rebuild the table using the filtered data
